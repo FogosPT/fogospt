79ef616c2ecfbda255547c31ce280711d558c538
tomahock <eu@tomahock.com>
2018-05-02 23:52:58 +0100

    analytics

--- fogospt/resources/views/index.blade.php
+++ fogospt/resources/views/index.blade.php
@@ -220,6 +220,17 @@
     {{--});--}}
     {{--</script>--}}
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-29689840-9"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-29689840-9');
+    </script>
+
+    
     </body>
     </html>
 
