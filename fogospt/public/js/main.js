6fe0281ead1a5744e0f3d62f508d72005cd2bc3d
tomahock <eu@tomahock.com>
2018-05-04 22:50:17 +0100

    risk today, concelhos layers

--- fogospt/public/js/main.js
+++ fogospt/public/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var mymap = L.map('map').setView([ 40.5050025, -7.9053189], 7);
+    var mymap = L.map('map').setView([40.5050025, -7.9053189], 7);
 
 
     L.tileLayer('https://api.mapbox.com/styles/v1/fogospt/cjgppvcdp00aa2spjclz9sjst/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZm9nb3NwdCIsImEiOiJjamZ3Y2E5OTMyMjFnMnFxbjAxbmt3bmdtIn0.xg1X-A17WRBaDghhzsmjIA', {
@@ -9,9 +9,11 @@ $(document).ready(function () {
         accessToken: 'pk.eyJ1IjoidG9tYWhvY2siLCJhIjoiY2pmYmgydHJnMzMwaTJ3azduYzI2eGZteiJ9.4Z0iB0Pgbb3M_8t9VG10kQ'
     }).addTo(mymap);
 
-    mymap.on('click',function (e) {
-       $('.sidebar').removeClass('active');
-       $('#map').find('.fa-map-marker-alt').removeClass('active').addClass('fa-map-marker').removeClass('fa-map-marker-alt');
+
+    addRisk(mymap);
+    mymap.on('click', function (e) {
+        $('.sidebar').removeClass('active');
+        $('#map').find('.fa-map-marker-alt').removeClass('active').addClass('fa-map-marker').removeClass('fa-map-marker-alt');
     });
 
     var url = 'https://fogos.pt/new/fires';
@@ -21,10 +23,10 @@ $(document).ready(function () {
         success: function (data) {
             console.log(data);
             data = JSON.parse(data);
-            if(data.success){
-                for(i in data.data){
+            if (data.success) {
+                for (i in data.data) {
                     var item = data.data[i];
-                    var coords = [ item.lat, item.lng ];
+                    var coords = [item.lat, item.lng];
 
                     var el = document.createElement('div');
                     el.className = 'marker';
@@ -36,14 +38,14 @@ $(document).ready(function () {
 
                     marker.setIcon(L.divIcon({
                         className: 'count-icon-emergency',
-                        html: '<i class="fas fa-map-marker map-marker status-'+ item.statusCode +'"></i>',
+                        html: '<i class="fas fa-map-marker map-marker status-' + item.statusCode + '"></i>',
                         iconSize: [40, 40]
                     }));
 
 
-                    marker.addTo(mymap).on('click',function (e) {
+                    marker.addTo(mymap).on('click', function (e) {
                         $('#map').find('.fa-map-marker-alt').removeClass('active').addClass('fa-map-marker').removeClass('fa-map-marker-alt');
-                        mymap.setView(e.latlng,10);
+                        mymap.setView(e.latlng, 10);
 
 
                         var $icon = $(e.target._icon);
@@ -227,8 +229,68 @@ function danger(id) {
 
 }
 
-function addPageview(){
-    if(window.ga){
+
+function getColor(d) {
+    return d === 1 ? '#509e2f' :
+        d === 2 ? '#ffe900' :
+            d === 3 ? '#e87722' :
+                d === 4 ? '#cb333b' :
+                    d === 5 ? '#6f263d' :
+                        'rgb(255,  255,  255)';
+}
+
+
+function addRisk(mymap){
+
+    var concelhosStyle = {
+        "color": "#ff7800",
+        "weight": 2,
+        "opacity": 1,
+        "stroke": true,
+        "fill": false
+    };
+
+
+    var concelhosLayer = L.geoJSON(concelhos, {
+        style: concelhosStyle
+    });
+
+
+    var url = 'https://fogos.pt/v1/risk-today';
+    $.ajax({
+        url: url,
+        method: 'GET',
+        success: function (data) {
+            data = JSON.parse(data);
+            if (data.success) {
+                var riscosHoje = L.geoJson(concelhos, {
+                    style: function (feature) {
+                        var d = data.data.local[feature.properties.DICO].data.rcm;
+                        return {weight: 1.0, color: '#666', fillOpacity: 0.6, fillColor: getColor(d)};
+                    },
+                });
+
+
+                overlayPane = {
+                    "Concelhos": concelhosLayer,
+                    "Risco Hoje": riscosHoje,
+                };
+
+                layerControl = L.control.layers(null, overlayPane, {position: 'topright'});
+                layerControl.addTo(mymap);
+            }
+        }
+    });
+
+
+
+
+
+
+}
+
+function addPageview() {
+    if (window.ga) {
         ga('send', 'pageview', location.pathname);
     }
 }
--- fogospt/resources/views/index.blade.php
+++ fogospt/resources/views/index.blade.php
@@ -32,6 +32,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 
+    <script src="/js/concelhos.js"></script>
     <script src="/js/main.js"></script>
 
     <title>Beta Fogos.pt</title>
