cbc6ef65975e89c2dca22599c49fb61ac074cecc
chateaufiesta <chateaufiesta@gmail.com>
2018-05-08 09:05:53 -0400

    Manifest page

---
+++ fogospt/resources/views/manifest.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    @include('includes.head')
+    @include('includes.meta')
+</head>
+<body>
+
+@include('includes.navbar')
+
+<main role="main" class="mb-auto">
+    <div>MANIFEST</div>
+</main>
+
+@include('includes.scripts')
+
+</body>
+</html>
