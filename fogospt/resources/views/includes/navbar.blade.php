51f638f2d222eff9ba590f9f3de299d1bff6bebf
isacosta <imscosta@gmail.com>
2018-05-09 00:02:04 +0100

    header fix

--- fogospt/resources/views/includes/navbar.blade.php
+++ fogospt/resources/views/includes/navbar.blade.php
@@ -1,20 +1,16 @@
 <header id="header" class="fixed-top" role="banner">
     <nav class="navbar navbar-dark">
-        <div class="container flex-column flex-sm-row justify-content-center justify-content-sm-between">
-            <div>
-                <a class="navbar-brand" href="/"><img src="/img/logo.svg" height="70px"></a>
+        <a class="navbar-brand" href="/"><img src="/img/logo.svg" height="70px"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuTopo" aria-controls="menuTopo" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="menuTopo">
+            <div class="navbar-nav float-right text-right pr-3">
+                <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(atual)</span></a>
+                <a class="nav-item nav-link" href="/sobre">Sobre</a>
+                <a class="nav-item nav-link" href="/informacoes">Informações</a>
+                <a class="nav-item nav-link" href="/manifesto">Manifesto</a>
             </div>
         </div>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuTopo" aria-controls="menuTopo" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-    <div class="collapse navbar-collapse" id="menuTopo">
-      <div class="navbar-nav float-right text-right pr-3">
-        <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(atual)</span></a>
-        <a class="nav-item nav-link" href="/sobre">Sobre</a>
-        <a class="nav-item nav-link" href="/informacoes">Informações</a>
-        <a class="nav-item nav-link" href="/manifesto">Manifesto</a>
-      </div>
-    </div>
     </nav>
 </header>
