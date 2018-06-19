10e89a63041347e095e149dda726dfffcf51305a
tomahock <eu@tomahock.com>
2018-06-19 20:45:42 +0100

    temperature units

--- fogospt/resources/views/elements/meteo.blade.php
+++ fogospt/resources/views/elements/meteo.blade.php
@@ -1,13 +1,13 @@
 @isset($fire['meteo']['main'])
     <div id="meteo">
         <div class="temp_atual">
-            @lang('elements.cards.meteo.temp_atual'):&nbsp;{{$fire['meteo']['main']['temp']}}
+            @lang('elements.cards.meteo.temp_atual'):&nbsp;{{$fire['meteo']['main']['temp']}}ºC
         </div>
         <div class="temp_min">
-            @lang('elements.cards.meteo.temp_min'):&nbsp;{{$fire['meteo']['main']['temp_min']}}
+            @lang('elements.cards.meteo.temp_min'):&nbsp;{{$fire['meteo']['main']['temp_min']}}ºC
         </div>
         <div class="temp_min">
-            @lang('elements.cards.meteo.temp_max'):&nbsp;{{$fire['meteo']['main']['temp_max']}}
+            @lang('elements.cards.meteo.temp_max'):&nbsp;{{$fire['meteo']['main']['temp_max']}}ºC
         </div>
         <div class="temp_min">
             @lang('elements.cards.meteo.estado_atual'):&nbsp;{{$fire['meteo']['weather'][0]['description']}}&nbsp; <img
