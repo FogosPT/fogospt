83187254de0db57aea5d4f53ee6ee5f48bcf89ce
tomahock <eu@tomahock.com>
2018-08-01 19:16:09 +0100

    preparing to prod

--- fogospt/resources/views/includes/meta.blade.php
+++ fogospt/resources/views/includes/meta.blade.php
@@ -2,16 +2,16 @@
 <meta name="Keywords"
       content="@lang('includes.meta.content')">
 <meta property="og:title" content="{{$metadata['title']}}">
-<meta property="og:site_name" content="beta.fogos.pt">
+<meta property="og:site_name" content="Fogos.pt">
 <meta property="og:url" content="{{$metadata['url']}}">
 <meta property="og:description" content="{{$metadata['description']}}">
 <meta property="og:type" content="website">
-<meta property="og:image" content="https://fogos.pt/assets/logo.png">
+<meta property="og:image" content="https://fogos.pt/img/logo.png">
 <meta property="fb:app_id" content="966421526811840">
 <meta name="twitter:card" content="summary">
 <meta name="twitter:site" content="@tomahock">
 <meta name="twitter:title" content="{{$metadata['title']}}">
 <meta name="twitter:description" content="{{$metadata['description']}}">
-<meta name="twitter:image" content="https://fogos.pt/assets/logo.png">
+<meta name="twitter:image" content="https://fogos.pt/img/logo.png">
 
-<title>beta.fogos.pt {{$metadata['pageTitle']}}</title>
+<title>Fogos.pt {{$metadata['pageTitle']}}</title>
