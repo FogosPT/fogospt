9b5ead490e3e5827380ddc940a871975445884c4
renato <renato.silva.an@gmail.com>
2019-02-24 20:03:21 +0000

    Remove commented code

--- fogospt/resources/views/partnerships.blade.php
+++ fogospt/resources/views/partnerships.blade.php
@@ -41,24 +41,6 @@
             </div>
             <div class="row  bg-white">
 
-              <!--  <div class="col-sm">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <a href="https://brpx.com">
-                                <img src="{{asset('img/partners/brpx.png')}}" alt="Bright Pixel">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <a href="https://www.mapbox.com">
-                                <img src="{{asset('img/partners/mapbox-logo-color.png')}}" alt="Mapbox">
-                            </a>
-                        </div>
-                    </div>
-                </div>-->
             </div>
         </div>
     </main>
