1d9ffc3f549c1d884d9bbbdc0e3ffad4a1007646
renato <renato.silva.an@gmail.com>
2019-02-27 20:56:12 +0000

    Change avatar size

--- fogospt/resources/views/elements/twitter.blade.php
+++ fogospt/resources/views/elements/twitter.blade.php
@@ -10,7 +10,7 @@
           <a  target="_blank" rel="noopener noreferrer" href="https://twitter.com/{{$tweet->user->screen_name}}/status/{{$tweet->id_str}}" class="list-group-item list-group-item-action flex-column align-items-start">
 
             <div class="d-flex w-100 justify-content-between">
-               <img src="https://pbs.twimg.com/profile_images/992860752624091136/f5ClxNvF_normal.jpg" style="width: 50px; height: 50px; border-radius: 50%;"> 
+               <img src="https://pbs.twimg.com/profile_images/992860752624091136/f5ClxNvF_normal.jpg" style="width: 25px; height: 25px; border-radius: 50%;"> 
               <h6 class="mb-1">{{$tweet->user->name}}</h6>
               <!--small class="text-muted">{{$tweet->created_at}}</small-->
             </div>
