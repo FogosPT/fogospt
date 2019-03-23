91d2c739b8f78c44e69af0c2c6a5e09beef955cc
João Pina <tomahock@users.noreply.github.com>
2019-03-23 16:55:15 +0000

    pco & cos

--- fogospt/resources/views/elements/extra.blade.php
+++ fogospt/resources/views/elements/extra.blade.php
@@ -1,3 +1,14 @@
 @isset($fire['extra'])
     <p>{{ $fire['extra'] }}</p>
 @endisset
+
+
+@isset($fire['cos'])
+    <h4 class="card-title">Comandante Operações de Socorro</h4>
+    <p>{{ $fire['cos'] }}</p>
+@endisset
+
+@isset($fire['pco'])
+    <h4 class="card-title">Posto de Comando Operacional</h4>
+    <p>{{ $fire['pco'] }}</p>
+@endisset
