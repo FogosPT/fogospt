a13b58db2b43fd4616d7c7d4056170d2b4985e88
tomahock <eu@tomahock.com>
2019-03-30 17:44:50 +0000

    fix lightning

--- fogospt/public/js/main.js
+++ fogospt/public/js/main.js
@@ -314,7 +314,7 @@ $(document).ready(function() {
 
 
   $.ajax({
-    url: 'http://127.0.0.1:8000/lightnings',
+    url: '/lightnings',
     dataType: "json",
     method: 'GET',
     success: function(data) {
