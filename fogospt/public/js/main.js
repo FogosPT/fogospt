547d3d7c3c0a12da21c48261c44443c1c2adaacf
Tomahock <eu@tomahock.com>
2021-05-24 17:42:35 +0100

    auto refresh data

--- fogospt/public/js/main.js
+++ fogospt/public/js/main.js
@@ -1,8 +1,6 @@
+var layerControl2 = null;
+
 $(document).ready(function () {
-
-
-
-
     const messaging = firebase.messaging()
 
     messaging.onMessage(function (payload) {
@@ -97,61 +95,7 @@ $(document).ready(function () {
     window.fogosLayers[12] = L.layerGroup()
     window.fogosLayers[80] = L.layerGroup()
 
-    var url = 'https://api-dev.fogos.pt/new/fires'
-    $.ajax({
-        url: url,
-        method: 'GET',
-        success: function (data) {
-            console.log(data);
-            //data = JSON.parse(data)
-            if (data.success) {
-
-                for (i in data.data) {
-                    calculateImportanceValue(data.data[i])
-                }
-                for (i in data.data) {
-                    addMaker(data.data[i], mymap)
-                }
-
-                var obj = {}
-                obj['Despacho'] = window.fogosLayers[3]
-                obj['Despacho de 1º Alerta'] = window.fogosLayers[4]
-                obj['Chegada ao TO'] = window.fogosLayers[6]
-                obj['Em Curso'] = window.fogosLayers[5]
-                obj['Em Resolução'] = window.fogosLayers[7]
-                obj['Conclusão'] = window.fogosLayers[8]
-                obj['Vigilância'] = window.fogosLayers[9]
-                obj['Encerrada'] = window.fogosLayers[10]
-                obj['Falso Alarme'] = window.fogosLayers[11]
-                obj['Falso Alerta'] = window.fogosLayers[12]
-
-                obj['Despacho'].addTo(mymap)
-                obj['Despacho de 1º Alerta'].addTo(mymap)
-                obj['Chegada ao TO'].addTo(mymap)
-                obj['Em Curso'].addTo(mymap)
-                obj['Em Resolução'].addTo(mymap)
-                obj['Conclusão'].addTo(mymap)
-                obj['Vigilância'].addTo(mymap)
-                obj['Encerrada'].addTo(mymap)
-                obj['Falso Alarme'].addTo(mymap)
-                obj['Falso Alerta'].addTo(mymap)
-
-                layerControl2 = L.control.layers(null, obj, {
-                    position: 'topright'
-                })
-
-                layerControl2.addTo(mymap)
-                $controls = $(layerControl2.getContainer())
-                $controls.find('a').css({
-                    'background-image': 'none',
-                    'font-size': '33px',
-                    'text-align': 'center',
-                    'color': '#333333'
-                }).append('<i class="fas fa-map-marker"></i>')
-
-            }
-        }
-    })
+    var obj = getNewFires(mymap);
 
 
     var cloudLayer = L.OWM.cloudsClassic({
@@ -296,13 +240,21 @@ $(document).ready(function () {
         url: 'https://api-dev.fogos.pt/v1/warnings/site',
         success: function (data) {
             if (data.success && data.data[0] && data.data[0].active) {
-                console.log('cenas')
                 $('#warning-site').find('.modal-body').html('<p>' + data.data[0].text + '</p>');
                 $('#warning-site').modal('show');
             }
         },
     });
 
+
+    $('.js-refresh').on('click',function(){
+        getNewFires(mymap, true);
+    });
+
+    setInterval(function (){
+        toastr.success('A actualizar dados...', {timeOut: 1000});
+        getNewFires(mymap, true);
+    },60000)
 
 })
 
@@ -533,7 +485,7 @@ function addMaker(item, mymap) {
 
     window.fogosLayers[item.statusCode].addLayer(marker)
 
-    marker.addTo(mymap)
+    marker.addTo(mymap);
     marker.id = item.id
 
     if (isActive && isActive === item.id) {
@@ -927,4 +879,89 @@ function changeElementSizeById(id, size) {
     //Set costum size
     markerHtml.style.height = size + 'px'
     markerHtml.style.width = size + 'px'
+}
+
+function getNewFires(mymap, refresh = false)
+{
+    if(refresh){
+        layerControl2.remove();
+        window.fogosLayers[3].remove()
+        window.fogosLayers[4].remove()
+        window.fogosLayers[5].remove()
+        window.fogosLayers[6].remove()
+        window.fogosLayers[7].remove()
+        window.fogosLayers[8].remove()
+        window.fogosLayers[9].remove()
+        window.fogosLayers[10].remove()
+        window.fogosLayers[11].remove()
+        window.fogosLayers[12].remove()
+        window.fogosLayers[80].remove()
+
+        window.fogosLayers = []
+        window.fogosLayers[3] = L.layerGroup()
+        window.fogosLayers[4] = L.layerGroup()
+        window.fogosLayers[5] = L.layerGroup()
+        window.fogosLayers[6] = L.layerGroup()
+        window.fogosLayers[7] = L.layerGroup()
+        window.fogosLayers[8] = L.layerGroup()
+        window.fogosLayers[9] = L.layerGroup()
+        window.fogosLayers[10] = L.layerGroup()
+        window.fogosLayers[11] = L.layerGroup()
+        window.fogosLayers[12] = L.layerGroup()
+        window.fogosLayers[80] = L.layerGroup()
+    }
+
+    var url = 'https://api-dev.fogos.pt/new/fires'
+    $.ajax({
+        url: url,
+        method: 'GET',
+        success: function (data) {
+            if (data.success) {
+
+                for (i in data.data) {
+                    calculateImportanceValue(data.data[i])
+                }
+                for (i in data.data) {
+                    addMaker(data.data[i], mymap)
+                }
+
+                var obj = {}
+                obj['Despacho'] = window.fogosLayers[3]
+                obj['Despacho de 1º Alerta'] = window.fogosLayers[4]
+                obj['Chegada ao TO'] = window.fogosLayers[6]
+                obj['Em Curso'] = window.fogosLayers[5]
+                obj['Em Resolução'] = window.fogosLayers[7]
+                obj['Conclusão'] = window.fogosLayers[8]
+                obj['Vigilância'] = window.fogosLayers[9]
+                obj['Encerrada'] = window.fogosLayers[10]
+                obj['Falso Alarme'] = window.fogosLayers[11]
+                obj['Falso Alerta'] = window.fogosLayers[12]
+
+                obj['Despacho'].addTo(mymap)
+                obj['Despacho de 1º Alerta'].addTo(mymap)
+                obj['Chegada ao TO'].addTo(mymap)
+                obj['Em Curso'].addTo(mymap)
+                obj['Em Resolução'].addTo(mymap)
+                obj['Conclusão'].addTo(mymap)
+                obj['Vigilância'].addTo(mymap)
+                obj['Encerrada'].addTo(mymap)
+                obj['Falso Alarme'].addTo(mymap)
+                obj['Falso Alerta'].addTo(mymap)
+
+                layerControl2 = L.control.layers(null, obj, {
+                    position: 'topright'
+                })
+
+                layerControl2.addTo(mymap)
+                $controls = $(layerControl2.getContainer())
+                $controls.find('a').css({
+                    'background-image': 'none',
+                    'font-size': '33px',
+                    'text-align': 'center',
+                    'color': '#333333'
+                }).append('<i class="fas fa-map-marker"></i>')
+            }
+        }
+    })
+
 }
