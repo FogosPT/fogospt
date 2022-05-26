5a2b6b7612e50265f2a094ba96726cfbbdb8635d
Tomahock <eu@tomahock.com>
2022-05-26 21:47:31 +0100

    ha

--- fogospt/resources/lang/pt/pages.php
+++ fogospt/resources/lang/pt/pages.php
@@ -91,7 +91,7 @@ return [
         'last-days' => 'Últimos dias',
         'last-night-footer' => 'Incêndios entre as 21h e as 09h',
         'burn-area-last-days' => 'Área ardida nos últimos dias',
-        'burn-area-last-days-footer' => 'Dados registados no ICNF, nem todos os incêndios têm estes dados'
+        'burn-area-last-days-footer' => 'Dados registados no ICNF, nem todos os incêndios têm estes dados. Em hectares.'
     ],
     'table' => [
         'reload' => 'Esta página é atualizada automaticamente.'
