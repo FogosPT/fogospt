c939b7f61b49334482f0e2445493648f86095b1c
Tomahock <eu@tomahock.com>
2025-08-11 10:16:17 +0100

    add ptservidor

--- fogospt/resources/views/partnerships.blade.php
+++ fogospt/resources/views/partnerships.blade.php
@@ -13,6 +13,21 @@
             <div class="card-deck mb-3 text-center">
                 <div class="card mb-4 shadow-sm">
                     <div class="card-header">
+                        <h4 class="my-0 font-weight-normal">PTServidor</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="card-body">
+                            <a href="https://ptservidor.pt/">
+                                <img src="{{asset('img/partners/ptservidor.png')}}" alt="PTServodor">
+                            </a>
+                        </div>
+                        <a type="button" href="https://ptservidor.pt/"
+                           class="btn btn-lg btn-block btn-primary">Visitar</a>
+                    </div>
+                </div>
+
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-header">
                         <h4 class="my-0 font-weight-normal">Officelan</h4>
                     </div>
                     <div class="card-body">
@@ -21,55 +36,44 @@
                                 <img src="{{asset('img/partners/officelan.png')}}" alt="Officelan">
                             </a>
                         </div>
-                        <a type="button"  href="https://officelan.pt/" class="btn btn-lg btn-block btn-primary">Visitar</a>
+                        <a type="button" href="https://officelan.pt/"
+                           class="btn btn-lg btn-block btn-primary">Visitar</a>
                     </div>
                 </div>
 
-            <div class="card-deck mb-3 text-center">
-                <div class="card mb-4 shadow-sm">
-                    <div class="card-header">
-                        <h4 class="my-0 font-weight-normal">VOST Portugal</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="card-body">
-                            <a href="https://vost.pt/">
-                                <img src="{{asset('img/partners/vostpt.png')}}" alt="VOST Portugal">
-                            </a>
+                <div class="card-deck mb-3 text-center">
+                    <div class="card mb-4 shadow-sm">
+                        <div class="card-header">
+                            <h4 class="my-0 font-weight-normal">VOST Portugal</h4>
                         </div>
-                        <a type="button"  href="https://vost.pt/" class="btn btn-lg btn-block btn-primary">Visitar</a>
+                        <div class="card-body">
+                            <div class="card-body">
+                                <a href="https://vost.pt/">
+                                    <img src="{{asset('img/partners/vostpt.png')}}" alt="VOST Portugal">
+                                </a>
+                            </div>
+                            <a type="button" href="https://vost.pt/"
+                               class="btn btn-lg btn-block btn-primary">Visitar</a>
+                        </div>
+                    </div>
+
+                    <div class="card mb-4 shadow-sm">
+                        <div class="card-header">
+                            <h4 class="my-0 font-weight-normal">MAPBOX</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-body">
+                                <a href="https://www.mapbox.com">
+                                    <img src="{{asset('img/partners/mapbox.png')}}" alt="Mapbox">
+                                </a>
+                            </div>
+                            <a type="button" href="https://www.mapbox.com" class="btn btn-lg btn-block btn-primary">Visitar</a>
+                        </div>
                     </div>
                 </div>
+                <div class="row  bg-white">
 
-              <div class="card mb-4 shadow-sm">
-                <div class="card-header">
-                  <h4 class="my-0 font-weight-normal">BRPX</h4>
                 </div>
-                <div class="card-body">
-                  <div class="card-body">
-                      <a href="https://brpx.com">
-                          <img src="{{asset('img/partners/brpx.png')}}" alt="Bright Pixel">
-                      </a>
-                  </div>
-                  <a type="button"  href="https://brpx.com" class="btn btn-lg btn-block btn-primary">Visitar</a>
-                </div>
-              </div>
-              <div class="card mb-4 shadow-sm">
-                <div class="card-header">
-                  <h4 class="my-0 font-weight-normal">MAPBOX</h4>
-                </div>
-                <div class="card-body">
-                  <div class="card-body">
-                      <a href="https://www.mapbox.com">
-                          <img src="{{asset('img/partners/mapbox.png')}}" alt="Mapbox">
-                      </a>
-                  </div>
-                  <a type="button" href="https://www.mapbox.com" class="btn btn-lg btn-block btn-primary">Visitar</a>
-                </div>
-              </div>
             </div>
-            <div class="row  bg-white">
-
-            </div>
-        </div>
     </main>
 @endsection
