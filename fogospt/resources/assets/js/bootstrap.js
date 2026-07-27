327fa221fd3a554c27b5b41cc9f23faf329d4be1
Tiago <tigasf25@gmail.com>
2026-07-27 15:18:09 +0100

    build: upgrade Laravel Mix v2->v5 for Node 18+ compatibility
    - laravel-mix ^2.1.14 -> ^5.0.0, cross-env ^5.2.0 -> ^7.0.3
    - Add sass, sass-loader, resolve-url-loader (Mix 5 requires)
    - Remove bootstrap, popper.js (Bootstrap 5 loaded via CDN)
    - Remove deprecated --hide-modules/--inline webpack-cli flags
    - Remove require('bootstrap') and require('popper.js') from bootstrap.js
    - Add Playwright build verification tests (6/6 passing)
    - Clean webpack.mix.js: remove boilerplate comments only

--- fogospt/resources/assets/js/bootstrap.js
+++ fogospt/resources/assets/js/bootstrap.js
@@ -1,17 +1,8 @@
 
 window._ = require('lodash');
-window.Popper = require('popper.js').default;
-
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
 
 try {
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
 } catch (e) {}
 
 /**
